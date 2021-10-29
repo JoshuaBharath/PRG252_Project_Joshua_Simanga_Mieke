df4eb79507934d6e6584de32e735a88444e9ea9a
JoshuaBharath <576802@student.belgiumcampus.ac.za>
2021-10-29 13:34:29 +0200

    Add project files.

---
+++ FileHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PRG252_Project_Joshua_Simanga_Mieke
+{
+    class FileHandler
+    {
+        string fileName = @"LoginCredentials.txt";
+
+        public void Write( string username, string password)
+        {
+            if (File.Exists(fileName))
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Append);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine($"{username}|{password}");
+                sw.Close();
+                fs.Close();
+            }
+            else
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Create);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine($"{username}|{password}");
+                sw.Close();
+                fs.Close();
+            }
+
+        }
+        // same here can you add the the sudent class where ever theirs Syntax error
+        public List<StudentCredentials> ReadCredentials()
+        {
+            List<StudentCredentials> readList = new List<StudentCredentials>();
+            using (StreamReader sr=new StreamReader(fileName))
+            {
+                string holder;
+                while ((holder=sr.ReadLine())!=null)
+                {
+                    string[] splitHolder = holder.Split('|');
+                    StudentCredentials sc = new StudentCredentials(splitHolder[0], splitHolder[1]);
+                    readList.Add(sc);
+                }
+            }
+            return readList;
+        }
+    }
+}
---
+++ Form7.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace PRG252_Project_Joshua_Simanga_Mieke
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtDob = new System.Windows.Forms.TextBox();
+            this.stdImg = new System.Windows.Forms.TextBox();
+            this.txtModuleCode = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStdName = new System.Windows.Forms.TextBox();
+            this.txtStdSurname = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(323, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Update";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Number";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(245, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // txtDob
+            // 
+            this.txtDob.Location = new System.Drawing.Point(366, 201);
+            this.txtDob.Name = "txtDob";
+            this.txtDob.Size = new System.Drawing.Size(183, 22);
+            this.txtDob.TabIndex = 24;
+            // 
+            // stdImg
+            // 
+            this.stdImg.Location = new System.Drawing.Point(49, 379);
+            this.stdImg.Name = "stdImg";
+            this.stdImg.Size = new System.Drawing.Size(183, 22);
+            this.stdImg.TabIndex = 23;
+            // 
+            // txtModuleCode
+            // 
+            this.txtModuleCode.Location = new System.Drawing.Point(595, 201);
+            this.txtModuleCode.Name = "txtModuleCode";
+            this.txtModuleCode.Size = new System.Drawing.Size(183, 22);
+            this.txtModuleCode.TabIndex = 22;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(585, 299);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(183, 22);
+            this.txtAddress.TabIndex = 20;
+            this.txtAddress.TextChanged += new System.EventHandler(this.txtAddress_TextChanged);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(366, 379);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(183, 22);
+            this.txtPhone.TabIndex = 18;
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(366, 296);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(183, 22);
+            this.txtGender.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(590, 160);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(159, 29);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Module Code";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(590, 249);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(102, 29);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Address";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(367, 347);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 29);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Phone";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(361, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 29);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Gender";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(367, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 29);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "DOB";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(44, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 29);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Student Image";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(44, 169);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(166, 29);
+            this.label13.TabIndex = 8;
+            this.label13.Text = "Student Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(198, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Student Surname";
+            // 
+            // txtStdName
+            // 
+            this.txtStdName.Location = new System.Drawing.Point(49, 219);
+            this.txtStdName.Name = "txtStdName";
+            this.txtStdName.Size = new System.Drawing.Size(183, 22);
+            this.txtStdName.TabIndex = 25;
+            // 
+            // txtStdSurname
+            // 
+            this.txtStdSurname.Location = new System.Drawing.Point(49, 296);
+            this.txtStdSurname.Name = "txtStdSurname";
+            this.txtStdSurname.Size = new System.Drawing.Size(183, 22);
+            this.txtStdSurname.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 38);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 54);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Back";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDob);
+            this.Controls.Add(this.stdImg);
+            this.Controls.Add(this.txtModuleCode);
+            this.Controls.Add(this.txtStdSurname);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtStdName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form7";
+            this.Text = "Form7";
+            this.Load += new System.EventHandler(this.Form7_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtDob;
+        private System.Windows.Forms.TextBox stdImg;
+        private System.Windows.Forms.TextBox txtModuleCode;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStdName;
+        private System.Windows.Forms.TextBox txtStdSurname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
